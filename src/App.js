0d3cb2e3b56c059f44008e1a00b4a9d8dbc7ca73
Fernando Gutierrez <fernandoezequiel333@gmail.com>
2023-03-28 19:05:08 -0300

    limpieza de main

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
-import {Login} from './Login'
 
 function App() {
   return (
     <div className="App">
-      <Login />
-      <h1>Hola desde React</h1>
+      
     </div>
   );
 }
