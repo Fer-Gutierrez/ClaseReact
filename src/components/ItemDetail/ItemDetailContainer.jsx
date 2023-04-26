dd23edbf05b0a41061bde5debbad7a406461f0c1
Fernando Gutierrez <fernandoezequiel333@gmail.com>
2023-04-25 21:14:04 -0300

    arreglo-ItemDetailContainer

--- src/components/ItemDetail/ItemDetailContainer.jsx
+++ src/components/ItemDetail/ItemDetailContainer.jsx
@@ -5,14 +5,14 @@ import { products } from "./../../productsMock";
 import { useParams } from "react-router-dom";
 
 const ItemDetailContainer = () => {
-  const [product, setProduct] = useState();
+  const [product, setProduct] = useState({});
 
   //useParams te trae siempre un objeto con propiedades, dichas propiedades tienen como key lo que le puse luego del /: en la ruta (en App.js)
   //Podemos desestructurar el objeto para acceder directamente a una o varias de sus propiedades.
   const { id } = useParams();
   //useParams te trae siempre un string dentro de cada propiedad, por lo tanto debemos convertirlo a numero para usar el find().
-  console.log(typeof id); 
-  
+  console.log(typeof id);
+
   useEffect(() => {
     let encontrado = products.find(prod => prod.id === +id); //Convertimos el id (string) en id (number).
     setTimeout(() => {
